89f55c4f816ce7201ca1d75b8169dc38e1057b5b
Dominic Hopton <d.hopton@codevoid.net>
2022-04-08 14:21:13 -0700

    Add test to validate workspace has a viable task
    Another test to double check the state of the workspace so we can make
    assumptions in later tests.
    
    Additional tweak: fixed description of the the serve task

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,10 +1,28 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
 
-suite("Extension Test Suite", () => {
-	vscode.window.showInformationMessage("Start all tests.");
+type Maybe<T> = T | null;
 
-	test("Sample Workspace was opened", () => {
-		assert.strictEqual(vscode.workspace.name, "sample-project");
-	});
+
+suite("Extension Test Suite", () => {
+    vscode.window.showInformationMessage("Start all tests.");
+
+    test("Sample workspace was opened", () => {
+        assert.strictEqual(vscode.workspace.name, "sample-project");
+    });
+
+    test("Opened workspace has appropriate serve task", async () => {
+        const foundTasks = await vscode.tasks.fetchTasks({ type: "npm" });
+        let serveTask: Maybe<vscode.Task> = null;
+
+        for (const task of foundTasks) {
+            if (!task.definition || task.definition.type !== "npm" || task.definition["script"] !== "serve") {
+                continue;
+            }
+
+            serveTask = task;
+        }
+
+        assert.ok(!!serveTask, "Serve task not found")
+    });
 });
