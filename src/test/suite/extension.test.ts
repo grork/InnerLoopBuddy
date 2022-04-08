1ac48e7585b5cb35414d27c3cc48b16cdd6d663d
Dominic Hopton <d.hopton@codevoid.net>
2022-04-08 15:22:02 -0700

    Add test to validate `serve` task starts
    - Added `test.json` to the sample project so it's easy to validate the right thing has come up
    - Added a `delay` helper in the test since it takes a 'moment' for the
    service to startup
    - Increased timeout from default for this test since it takes longer
    than two seconds to start
    - Trimmed the copy commands in the sample project

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,7 +1,14 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
+import fetch from "node-fetch";
 
 type Maybe<T> = T | null;
+
+const SERVE_TASK_TYPE = "npm";
+const SERVE_TASK_SCRIPT = "serve";
+
+// Helper to allow 'sleeping' in a test
+function delay(ms: number) { return new Promise(resolve => setTimeout(resolve, ms)) }
 
 
 suite("Extension Test Suite", () => {
@@ -16,7 +23,7 @@ suite("Extension Test Suite", () => {
         let serveTask: Maybe<vscode.Task> = null;
 
         for (const task of foundTasks) {
-            if (!task.definition || task.definition.type !== "npm" || task.definition["script"] !== "serve") {
+            if (!task.definition || task.definition.type !== SERVE_TASK_TYPE || task.definition["script"] !== SERVE_TASK_SCRIPT) {
                 continue;
             }
 
@@ -25,4 +32,24 @@ suite("Extension Test Suite", () => {
 
         assert.ok(!!serveTask, "Serve task not found")
     });
+
+    test("Execute Serve Task & Terminate", async () => {
+        const foundTasks = await vscode.tasks.fetchTasks({ type: "npm" });
+        let serveTask: Maybe<vscode.Task> = null;
+
+        for (const task of foundTasks) {
+            if (!task.definition || task.definition.type !== SERVE_TASK_TYPE || task.definition["script"] !== SERVE_TASK_SCRIPT) {
+                continue;
+            }
+
+            serveTask = task;
+        }
+
+        const runningTask = await vscode.tasks.executeTask(<vscode.Task>serveTask);
+        await delay(5000); // Wait for the service to actually startup
+        const testData: { readyForTest: boolean } = <any>await (await fetch("http://localhost:3000/test.json")).json();
+        
+        assert.ok(testData.readyForTest);
+        runningTask.terminate();
+    }).timeout(20 * 1000);
 });
